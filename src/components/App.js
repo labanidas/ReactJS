6e9c9801c6a978725c71b087dc8767d9c31e4c4d
Labani Das <labanidas2001@gmail.com>
2023-03-08 20:29:57 +0530

    basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,55 @@
-import '../App.css';
-import {v4 as uuid} from 'uuid';
-import React, {useState, useEffect} from 'react';
-import Header from './Header';
-import ContactList from './ContactList';
-import AddContact from './AddContact';
-
-
-  // console.log(process.env.REACT_APP_LOCAL_STORAGE_KEY);
+import "../App.css";
+import { v4 as uuid } from "uuid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import ContactList from "./ContactList";
+import AddContact from "./AddContact";
 
 function App() {
-
   const [contacts, setContacts] = useState([]);
-  
-  function addContact(contact){
-    setContacts( [...contacts, {id : uuid(), ...contact}]);
+
+  function addContact(contact) {
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   }
 
-  useEffect(()=>{
-    const newContacts = JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCAL_STORAGE_KEY));
-    if(newContacts.length!=0) setContacts(newContacts);
-  }, [])
+  useEffect(() => {
+    const newContacts = JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCAL_STORAGE_KEY)
+    );
+    if (newContacts.length != 0) setContacts(newContacts);
+  }, []);
 
-
-  function deleteContact(id){
-    const filteredContacts = contacts.filter((contact)=>{
-      return contact.id!== id
-    })
-    setContacts(filteredContacts)
+  function deleteContact(id) {
+    const filteredContacts = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(filteredContacts);
   }
 
-  useEffect(()=>{
-    localStorage.setItem(process.env.REACT_APP_LOCAL_STORAGE_KEY, JSON.stringify(contacts))
-  }, [contacts])
-
-
-  
-
-  
+  useEffect(() => {
+    localStorage.setItem(
+      process.env.REACT_APP_LOCAL_STORAGE_KEY,
+      JSON.stringify(contacts)
+    );
+  }, [contacts]);
 
   return (
-    <div className='ui container'>
-      <Header />
-      <AddContact onAdd = {addContact}/>
-      <ContactList contacts={contacts} deleteContact={deleteContact}/>
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ContactList contacts={contacts} deleteContact={deleteContact} />
+            }
+          />
+          <Route path="/add" element={<AddContact onAdd={addContact} />} />
+        </Routes>
+        {/* <AddContact onAdd = {addContact}/>
+      <ContactList contacts={contacts} deleteContact={deleteContact}/> */}
+      </Router>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,6 +4,20 @@ import ContactCard from './ContactCard';
 
 export default function ContactList(props){
 
+    
+  const contacts = [
+    {   
+      id:1,
+      name:'Labani Das',
+      email: 'labani@gmail.com'
+    },
+    {
+      id:2,
+      name:'Rima Das',
+      email: 'rima@gmail.com'
+    }
+  ]
+
 return(
     <>
     {props.contacts.map(contact =>(
